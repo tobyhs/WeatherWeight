b6138eb80a8b983571fe70b63b509795dbafe668
Toby Hsieh <thrasher13b@gmail.com>
2017-02-08 00:31:32 -0800

    Add test.WeatherResponseFactory
    This will be used to create WeatherResponse objects for testing.

---
+++ app/src/test/java/io/github/tobyhs/weatherweight/test/WeatherResponseFactory.java
@@ -0,0 +1,41 @@
+package io.github.tobyhs.weatherweight.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.github.tobyhs.weatherweight.yahooweather.model.Channel;
+import io.github.tobyhs.weatherweight.yahooweather.model.Item;
+import io.github.tobyhs.weatherweight.yahooweather.model.Location;
+import io.github.tobyhs.weatherweight.yahooweather.model.Query;
+import io.github.tobyhs.weatherweight.yahooweather.model.Results;
+import io.github.tobyhs.weatherweight.yahooweather.model.SingleForecast;
+import io.github.tobyhs.weatherweight.yahooweather.model.WeatherResponse;
+
+/**
+ * @see .create()
+ */
+public class WeatherResponseFactory {
+    /**
+     * This creates a valid {@link WeatherResponse} object for testing.
+     *
+     * @return a valid {@link WeatherResponse} for testing
+     */
+    public static WeatherResponse create() {
+        List<SingleForecast> forecast = Arrays.asList(
+                new SingleForecast().setDate("05 Feb 2017")
+                        .setLow("50").setHigh("55").setText("Showers"),
+                new SingleForecast().setDate("06 Feb 2017")
+                        .setLow("60").setHigh("63").setText("Cloudy"),
+                new SingleForecast().setDate("07 Feb 2017")
+                        .setLow("72").setHigh("76").setText("Sunny")
+        );
+
+        Location location = new Location()
+                .setCity("Oakland").setRegion("CA").setCountry("United States");
+        Item item = new Item().setForecast(forecast);
+        Channel channel = new Channel().setItem(item).setLocation(location);
+        Results results = new Results().setChannel(channel);
+
+        return new WeatherResponse().setQuery(new Query().setResults(results));
+    }
+}
