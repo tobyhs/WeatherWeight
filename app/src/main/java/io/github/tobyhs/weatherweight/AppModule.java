ab73c3d2de8d752a5b7aa79d3c5420afad7effe0
Toby Hsieh <thrasher13b@gmail.com>
2017-02-18 23:20:59 -0800

    Implement AppModule#provideWeatherRepository

--- app/src/main/java/io/github/tobyhs/weatherweight/AppModule.java
+++ app/src/main/java/io/github/tobyhs/weatherweight/AppModule.java
@@ -1,11 +1,18 @@
 package io.github.tobyhs.weatherweight;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
 
+import com.github.aurae.retrofit2.LoganSquareConverterFactory;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import dagger.Module;
 import dagger.Provides;
+import retrofit2.Retrofit;
 
 import io.github.tobyhs.weatherweight.util.SchedulerProvider;
+import io.github.tobyhs.weatherweight.yahooweather.WeatherRepository;
+import io.github.tobyhs.weatherweight.yahooweather.WeatherRepositoryImpl;
+import io.github.tobyhs.weatherweight.yahooweather.WeatherService;
 
 /**
  * Dagger module to provide common dependencies
@@ -37,5 +44,38 @@ public class AppModule {
     @Singleton
     SchedulerProvider provideSchedulerProvider() {
         return new SchedulerProvider();
+    }
+
+    /**
+     * @return a Retrofit instance for Yahoo's public API
+     */
+    @Provides @Named("yahooRetrofit")
+    @Singleton
+    Retrofit provideYahooRetrofit() {
+        return new Retrofit.Builder()
+                .baseUrl("https://query.yahooapis.com/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(LoganSquareConverterFactory.create())
+                .build();
+    }
+
+    /**
+     * @param retrofit a Retrofit instance for Yahoo's public API
+     * @return Retrofit service object for querying weather info from Yahoo's API
+     */
+    @Provides
+    @Singleton
+    WeatherService provideWeatherService(@Named("yahooRetrofit") Retrofit retrofit) {
+        return retrofit.create(WeatherService.class);
+    }
+
+    /**
+     * @param weatherService Retrofit service object for querying weather data from Yahoo's API
+     * @return repository object to fetch weather data from Yahoo's API
+     */
+    @Provides
+    @Singleton
+    WeatherRepository provideWeatherRepository(WeatherService weatherService) {
+        return new WeatherRepositoryImpl(weatherService);
     }
 }
--- app/src/test/java/io/github/tobyhs/weatherweight/AppModuleTest.java
+++ app/src/test/java/io/github/tobyhs/weatherweight/AppModuleTest.java
@@ -1,15 +1,24 @@
 package io.github.tobyhs.weatherweight;
 
+import com.github.aurae.retrofit2.LoganSquareConverterFactory;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import retrofit2.Retrofit;
 
 import io.github.tobyhs.weatherweight.test.BaseTestCase;
 import io.github.tobyhs.weatherweight.util.SchedulerProvider;
+import io.github.tobyhs.weatherweight.yahooweather.WeatherRepository;
+import io.github.tobyhs.weatherweight.yahooweather.WeatherRepositoryImpl;
+import io.github.tobyhs.weatherweight.yahooweather.WeatherService;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.isA;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class AppModuleTest extends BaseTestCase {
     @Mock private App app;
@@ -24,5 +33,26 @@ public class AppModuleTest extends BaseTestCase {
     public void testProvideSchedulerProvider() {
         Class schedulerProviderCls = module.provideSchedulerProvider().getClass();
         assertThat(schedulerProviderCls, is(equalTo((Class) SchedulerProvider.class)));
+    }
+
+    @Test
+    public void testProvideYahooRetrofit() {
+        Retrofit retrofit = module.provideYahooRetrofit();
+        assertThat(retrofit.baseUrl().toString(), is("https://query.yahooapis.com/"));
+        assertThat(retrofit.callAdapterFactories(), hasItem(isA(RxJava2CallAdapterFactory.class)));
+        assertThat(retrofit.converterFactories(), hasItem(isA(LoganSquareConverterFactory.class)));
+    }
+
+    @Test
+    public void testProvideWeatherService() {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://localhost/").build();
+        assertThat(module.provideWeatherService(retrofit), isA(WeatherService.class));
+    }
+
+    @Test
+    public void testProvideWeatherRepository() {
+        WeatherService weatherService = mock(WeatherService.class);
+        WeatherRepository repo = module.provideWeatherRepository(weatherService);
+        assertThat(repo.getClass(), is(equalTo((Class) WeatherRepositoryImpl.class)));
     }
 }
