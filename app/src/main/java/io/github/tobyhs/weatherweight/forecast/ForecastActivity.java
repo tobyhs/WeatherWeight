a2ff02287c68a1ae44f7631d110df05ac7e3aba5
Toby Hsieh <thrasher13b@gmail.com>
2017-02-22 22:55:07 -0800

    Add ForecastComponent to ForecastActivity

--- app/src/main/java/io/github/tobyhs/weatherweight/forecast/ForecastActivity.java
+++ app/src/main/java/io/github/tobyhs/weatherweight/forecast/ForecastActivity.java
@@ -1,17 +1,24 @@
 package io.github.tobyhs.weatherweight.forecast;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import io.github.tobyhs.weatherweight.App;
 import io.github.tobyhs.weatherweight.R;
 
 /**
  * Main activity to enter a location to retrieve a weather forecast
  */
 public class ForecastActivity extends AppCompatActivity {
+    private ForecastComponent forecastComponent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+
+        forecastComponent = DaggerForecastComponent.builder()
+                .appComponent(((App) getApplication()).getAppComponent())
+                .build();
     }
 }
