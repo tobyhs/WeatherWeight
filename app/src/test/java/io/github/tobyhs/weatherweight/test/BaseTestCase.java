4026503eae1d6c834fc30874e1ee1693afdf981e
Toby Hsieh <thrasher13b@gmail.com>
2017-11-07 19:53:31 -0800

    Use a Mockito JUnit rule in BaseTestCase

--- app/src/test/java/io/github/tobyhs/weatherweight/test/BaseTestCase.java
+++ app/src/test/java/io/github/tobyhs/weatherweight/test/BaseTestCase.java
@@ -1,14 +1,13 @@
 package io.github.tobyhs.weatherweight.test;
 
-import org.junit.Before;
-import org.mockito.MockitoAnnotations;
+import org.junit.Rule;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.quality.Strictness;
 
 /**
  * Base abstract test class to hold common hooks
  */
 public abstract class BaseTestCase {
-    @Before
-    public void initMocks() {
-        MockitoAnnotations.initMocks(this);
-    }
+    @Rule public MockitoRule mockito = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
 }
