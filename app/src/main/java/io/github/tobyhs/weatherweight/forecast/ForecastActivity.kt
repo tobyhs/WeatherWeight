4b6976235e1421c657b1f22ed54ca8464b689b99
Toby Hsieh <thrasher13b@gmail.com>
2021-07-25 00:47:02 -0700

    Convert ForecastActivity to Kotlin

---
+++ app/src/main/java/io/github/tobyhs/weatherweight/forecast/ForecastActivity.kt
@@ -0,0 +1,120 @@
+package io.github.tobyhs.weatherweight.forecast
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
+
+import com.hannesdorfmann.mosby3.mvp.viewstate.lce.LceViewState
+import com.hannesdorfmann.mosby3.mvp.viewstate.lce.MvpLceViewStateActivity
+import com.hannesdorfmann.mosby3.mvp.viewstate.lce.data.RetainingLceViewState
+
+import dagger.Lazy
+import dagger.android.AndroidInjection
+
+import io.github.tobyhs.weatherweight.data.model.ForecastResultSet
+import io.github.tobyhs.weatherweight.databinding.ActivityForecastBinding
+
+import org.threeten.bp.format.DateTimeFormatter
+
+import javax.inject.Inject
+
+/**
+ * Main activity to enter a location to retrieve a weather forecast
+ */
+class ForecastActivity :
+    MvpLceViewStateActivity<LinearLayout, ForecastResultSet, ForecastContract.View, ForecastPresenter>(),
+    ForecastContract.View, SearchView.OnQueryTextListener, OnRefreshListener {
+    @Inject
+    lateinit var lazyPresenter: Lazy<ForecastPresenter>
+
+    lateinit var binding: ActivityForecastBinding
+    private lateinit var forecastCardAdapter: ForecastCardAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+        super.onCreate(savedInstanceState)
+        binding = ActivityForecastBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initializeViews()
+
+        if (savedInstanceState == null) {
+            getPresenter().loadLastForecast()
+        }
+    }
+
+    override fun createPresenter(): ForecastPresenter {
+        return lazyPresenter.get()
+    }
+
+    override fun loadData(pullToRefresh: Boolean) {
+    }
+
+    override fun getData(): ForecastResultSet {
+        return getPresenter().forecastResultSet
+    }
+
+    override fun setData(forecastResultSet: ForecastResultSet?) {
+        if (forecastResultSet == null) {
+            return
+        }
+
+        binding.locationFound.text = forecastResultSet.location
+        val pubDate = forecastResultSet.publicationTime
+        binding.pubDate.text = pubDate.format(DateTimeFormatter.RFC_1123_DATE_TIME)
+        binding.forecastSwipeContainer.isRefreshing = false
+        forecastCardAdapter.set(forecastResultSet.forecasts)
+    }
+
+    public override fun getErrorMessage(e: Throwable, pullToRefresh: Boolean): String {
+        return e.toString()
+    }
+
+    override fun createViewState(): LceViewState<ForecastResultSet, ForecastContract.View> {
+        return RetainingLceViewState()
+    }
+
+    override fun setLocationInputText(location: String) {
+        binding.locationSearch.setQuery(location, false)
+        // I don't want locationSearch to have focus after loading the last forecast because it
+        // will bring up the keyboard, so I'll force focus on another view
+        binding.forecastRecyclerView.requestFocus()
+    }
+
+    override fun onQueryTextSubmit(query: String): Boolean {
+        getPresenter().search(query)
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String): Boolean {
+        return true
+    }
+
+    override fun onRefresh() {
+        getPresenter().search(binding.locationSearch.query.toString())
+    }
+
+    /**
+     * Sets up views in the activity layout
+     */
+    private fun initializeViews() {
+        binding.locationSearch.setOnQueryTextListener(this)
+        binding.locationSearch.isSubmitButtonEnabled = true
+
+        forecastCardAdapter = ForecastCardAdapter()
+        binding.forecastRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.forecastRecyclerView.adapter = forecastCardAdapter
+
+        binding.forecastSwipeContainer.setOnRefreshListener(this)
+        binding.accuweatherLogo.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, ACCUWEATHER_URI))
+        }
+    }
+
+    companion object {
+        private val ACCUWEATHER_URI = Uri.parse("https://www.accuweather.com")
+    }
+}
---
+++ app/src/test/java/io/github/tobyhs/weatherweight/forecast/ForecastActivityTest.kt
@@ -0,0 +1,185 @@
+package io.github.tobyhs.weatherweight.forecast
+
+import android.content.Intent
+import android.net.Uri
+import android.widget.TextView
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import com.hannesdorfmann.mosby3.mvp.viewstate.lce.data.RetainingLceViewState
+
+import io.github.tobyhs.weatherweight.R
+import io.github.tobyhs.weatherweight.test.ForecastResultSetFactory
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert.assertThat
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito
+import org.mockito.Mockito.clearInvocations
+import org.mockito.Mockito.never
+import org.mockito.Mockito.verify
+
+import org.robolectric.Shadows.shadowOf
+
+@RunWith(AndroidJUnit4::class)
+class ForecastActivityTest {
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(ForecastActivity::class.java)
+
+    @Test
+    fun onCreate() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            assertThat(activity.binding.locationSearch.isSubmitButtonEnabled, equalTo(true))
+        }
+    }
+
+    @Test
+    fun onCreateWithNullSavedInstanceState() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            verify(activity.presenter).loadLastForecast()
+        }
+    }
+
+    @Test
+    fun onCreateWithPresentSavedInstanceState() {
+        val scenario = activityScenarioRule.scenario
+        scenario.onActivity { activity ->
+            // Clearing so we can check loadLastForecast isn't called on the 2nd onCreate
+            clearInvocations(activity.presenter)
+        }
+
+        scenario.recreate()
+        scenario.onActivity { activity ->
+            verify(activity.presenter, never()).loadLastForecast()
+        }
+    }
+
+    @Test
+    fun createPresenter() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            assertThat(activity.createPresenter(), notNullValue())
+        }
+    }
+
+    @Test
+    fun getData() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            val forecastResultSet = ForecastResultSetFactory.create()
+            Mockito.`when`(activity.presenter.forecastResultSet).thenReturn(forecastResultSet)
+
+            assertThat(activity.data, equalTo(forecastResultSet))
+        }
+    }
+
+    @Test
+    fun setData() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            val binding = activity.binding
+            binding.forecastSwipeContainer.isRefreshing = true
+            val forecastResultSet = ForecastResultSetFactory.create()
+            activity.data = forecastResultSet
+
+            assertThat(binding.locationFound.text.toString(), equalTo("Oakland, CA, US"))
+            assertThat(binding.pubDate.text.toString(), equalTo("Fri, 1 Feb 2019 12:00:00 GMT"))
+            assertThat(binding.forecastSwipeContainer.isRefreshing, equalTo(false))
+
+            val recyclerView = binding.forecastRecyclerView
+            recyclerView.measure(0, 0)
+            recyclerView.layout(0, 0, 0, 10000)
+            val layoutManager = recyclerView.layoutManager!!
+
+            val forecasts = forecastResultSet.forecasts
+            assertThat(layoutManager.itemCount, equalTo(forecasts.size))
+
+            val view = layoutManager.findViewByPosition(0)!!
+            val dayView = view.findViewById<TextView>(R.id.day)
+            assertThat(dayView.text.toString(), equalTo("Fri"))
+            val dateView = view.findViewById<TextView>(R.id.date)
+            assertThat(dateView.text.toString(), equalTo("Feb 1"))
+            val lowView = view.findViewById<TextView>(R.id.temperatureLow)
+            assertThat(lowView.text.toString(), equalTo("60"))
+            val highView = view.findViewById<TextView>(R.id.temperatureHigh)
+            assertThat(highView.text.toString(), equalTo("65"))
+            val descriptionView = view.findViewById<TextView>(R.id.description)
+            assertThat(descriptionView.text.toString(), equalTo("Cloudy"))
+        }
+    }
+
+    @Test
+    fun setDataWithNull() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            // To check that a NullPointerException isn't thrown
+            activity.setData(null)
+        }
+    }
+
+    @Test
+    fun getErrorMessage() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            val throwable = Throwable("test error")
+            assertThat(activity.getErrorMessage(throwable, false), equalTo(throwable.toString()))
+        }
+    }
+
+    @Test
+    fun createViewState() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            assertThat(activity.createViewState(), instanceOf(RetainingLceViewState::class.java))
+        }
+    }
+
+    @Test
+    fun setLocationInputText() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            val location = "Saved City, SC"
+            activity.setLocationInputText(location)
+
+            assertThat(activity.binding.locationSearch.query.toString(), equalTo(location))
+            assertThat(activity.binding.locationSearch.hasFocus(), equalTo(false))
+            verify(activity.presenter, never()).search(anyString())
+        }
+    }
+
+    @Test
+    fun onQueryTextSubmit() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            val location = "San Francisco, CA"
+            activity.binding.locationSearch.setQuery(location, true)
+            verify(activity.presenter).search(location)
+        }
+    }
+
+    @Test
+    fun onQueryTextChange() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            assertThat(activity.onQueryTextChange("i"), equalTo(true))
+        }
+    }
+
+    @Test
+    fun onRefresh() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            val location = "Current"
+            activity.binding.locationSearch.setQuery(location, false)
+            activity.onRefresh()
+            verify(activity.presenter).search(location)
+        }
+    }
+
+    @Test
+    fun openAttributionUrl() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            activity.binding.accuweatherLogo.performClick()
+            val actual = shadowOf(activity).nextStartedActivity
+            val expected = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.accuweather.com"))
+            assertThat(actual.filterEquals(expected), equalTo(true))
+        }
+    }
+}
