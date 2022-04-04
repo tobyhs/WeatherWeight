62b76f841dcc62475b9132d023edc02a07d91d98
Toby Hsieh <thrasher13b@gmail.com>
2022-04-03 22:39:40 -0700

    Create ZonedDateTimeAdapter
    This is a Moshi adapter for java.util.ZonedDateTime objects.

---
+++ app/src/main/java/io/github/tobyhs/weatherweight/util/ZonedDateTimeAdapter.kt
@@ -0,0 +1,30 @@
+package io.github.tobyhs.weatherweight.util
+
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+/**
+ * Moshi adapter for java.time.ZonedDateTime objects
+ */
+class ZonedDateTimeAdapter {
+    /**
+     * Converts an ISO 8601 date-time into a java.time.ZonedDateTime
+     *
+     * @param string an ISO 8601 formatted date-time with time zone data
+     * @return the resulting ZonedDateTime object
+     */
+    @FromJson fun fromJson(string: String): ZonedDateTime = ZonedDateTime.parse(string)
+
+    /**
+     * Serializes a java.util.ZonedDateTime
+     *
+     * @param time the ZonedDateTime object to serialize
+     * @return a serialized string
+     */
+    @ToJson fun toJson(time: ZonedDateTime): String {
+        return time.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+    }
+}
---
+++ app/src/test/java/io/github/tobyhs/weatherweight/util/ZoneDateTimeAdapterTest.kt
@@ -0,0 +1,31 @@
+package io.github.tobyhs.weatherweight.util
+
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+
+import org.junit.Test
+
+class ZoneDateTimeAdapterTest {
+    private val adapter = ZonedDateTimeAdapter()
+
+    @Test
+    fun fromJson() {
+        val time = adapter.fromJson("2019-01-13T16:55:40-08:00")
+        assertThat(time.year, equalTo(2019))
+        assertThat(time.monthValue, equalTo(1))
+        assertThat(time.dayOfMonth, equalTo(13))
+        assertThat(time.hour, equalTo(16))
+        assertThat(time.minute, equalTo(55))
+        assertThat(time.second, equalTo(40))
+        assertThat(time.offset, equalTo(ZoneOffset.ofHours(-8)))
+    }
+
+    @Test
+    fun toJson() {
+        val time = ZonedDateTime.of(2022, 3, 30, 23, 40, 50, 0, ZoneOffset.ofHours(-8))
+        assertThat(adapter.toJson(time), equalTo("2022-03-30T23:40:50-08:00"))
+    }
+}
