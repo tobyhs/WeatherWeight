c6b97c99a6bade049d5ed97278fd72d252b899d5
Toby Hsieh <thrasher13b@gmail.com>
2022-05-17 19:19:21 -0700

    Implement FileLastForecastStore
    This is an implementation of LastForecastStore that uses a local file.
    It will be used with the app's designated cache directory.

---
+++ app/src/main/java/io/github/tobyhs/weatherweight/storage/FileLastForecastStore.kt
@@ -0,0 +1,38 @@
+package io.github.tobyhs.weatherweight.storage
+
+import com.squareup.moshi.Moshi
+
+import io.github.tobyhs.weatherweight.data.model.ForecastSearch
+
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Maybe
+
+import java.io.File
+
+/**
+ * An implementation of [LastForecastStore] that uses a local file
+ */
+class FileLastForecastStore
+/**
+ * @param directory the directory where the file will reside
+ * @param moshi Moshi instance to serialize data
+ */(internal val directory: File, internal val moshi: Moshi) : LastForecastStore {
+    private val file by lazy { File(directory, "lastForecast.json") }
+    private val moshiAdapter by lazy { moshi.adapter(ForecastSearch::class.java) }
+
+    override fun get(): Maybe<ForecastSearch> {
+        return Maybe.fromCallable {
+            if (file.exists()) {
+                moshiAdapter.fromJson(file.readText())
+            } else {
+                null
+            }
+        }
+    }
+
+    override fun save(forecastSearch: ForecastSearch): Completable {
+        return Completable.fromAction {
+            file.writeText(moshiAdapter.toJson(forecastSearch))
+        }
+    }
+}
---
+++ app/src/test/java/io/github/tobyhs/weatherweight/storage/FileLastForecastStoreTest.kt
@@ -0,0 +1,41 @@
+package io.github.tobyhs.weatherweight.storage
+
+import io.github.tobyhs.weatherweight.AppModule
+import io.github.tobyhs.weatherweight.data.model.ForecastSearch
+import io.github.tobyhs.weatherweight.test.ForecastSearchFactory
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+
+import org.junit.Test
+
+import java.io.File
+
+class FileLastForecastStoreTest {
+    private val directory = File("/tmp")
+    private val moshi = AppModule.provideMoshi()
+    private val forecastSearchAdapter = moshi.adapter(ForecastSearch::class.java)
+    private val store = FileLastForecastStore(directory, moshi)
+    private val file = File(directory, "lastForecast.json")
+    private val forecastSearch = ForecastSearchFactory.create()
+
+    @Test
+    fun `get when file does not exist`() {
+        file.delete()
+        assertThat(store.get().blockingGet(), nullValue())
+    }
+
+    @Test
+    fun `get when file exists`() {
+        file.writeText(forecastSearchAdapter.toJson(forecastSearch))
+        assertThat(store.get().blockingGet(), equalTo(forecastSearch))
+    }
+
+    @Test
+    fun save() {
+        file.delete()
+        store.save(forecastSearch).blockingAwait()
+        assertThat(forecastSearchAdapter.fromJson(file.readText()), equalTo(forecastSearch))
+    }
+}
