b814bfa7893da388e81f6130376b64e7f5e84938
Toby Hsieh <thrasher13b@gmail.com>
2023-02-12 20:56:11 -0800

    Create ForecastScreen composable

---
+++ app/src/main/java/io/github/tobyhs/weatherweight/forecast/ForecastScreen.kt
@@ -0,0 +1,138 @@
+package io.github.tobyhs.weatherweight.forecast
+
+import android.content.Intent
+import android.net.Uri
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+import io.github.tobyhs.weatherweight.R
+import io.github.tobyhs.weatherweight.data.model.ForecastResultSet
+import io.github.tobyhs.weatherweight.ui.LoadState
+
+/**
+ * Main composable for [ForecastActivity]
+ *
+ * @param viewModel
+ */
+@Composable
+fun ForecastScreen(viewModel: ForecastViewModel) {
+    val forecastState by viewModel.forecastState.observeAsState()
+    val locationInput by viewModel.locationInput.observeAsState("")
+
+    ForecastScreen(
+        forecastState = forecastState,
+        locationInput = locationInput,
+        onLocationInputChange = { viewModel.locationInput.value = it },
+        onSubmit = viewModel::search,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ForecastScreen(
+    forecastState: LoadState<ForecastResultSet>?,
+    locationInput: String = "",
+    onLocationInputChange: (String) -> Unit = {},
+    onSubmit: () -> Unit = {},
+) {
+    Column(Modifier.padding(all = dimensionResource(R.dimen.forecast_screen_padding))) {
+        val inputContentDescription = stringResource(R.string.locationSearchHint)
+        OutlinedTextField(
+            value = locationInput,
+            onValueChange = { onLocationInputChange(it) },
+            label = { Text(stringResource(R.string.location)) },
+            singleLine = true,
+            leadingIcon = {
+                IconButton(onClick = onSubmit) {
+                    Icon(
+                        Icons.Filled.Search,
+                        contentDescription = stringResource(R.string.submitLocation)
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+            keyboardActions = KeyboardActions(onSearch = { onSubmit() }),
+            modifier = Modifier.semantics { contentDescription = inputContentDescription }
+        )
+        Spacer(Modifier.height(8.dp))
+
+        Box(Modifier.fillMaxWidth().weight(1f)) {
+            when (forecastState) {
+                is LoadState.Loading -> {
+                    CircularProgressIndicator(
+                        Modifier.align(Alignment.TopCenter).testTag("loading")
+                    )
+                }
+                is LoadState.Content -> ForecastScreenContent(forecastState.content)
+                is LoadState.Error -> Text(
+                    forecastState.error.message.toString(),
+                    Modifier.testTag("error")
+                )
+                null -> {}
+            }
+        }
+
+        Text(stringResource(R.string.powered_by))
+        val context = LocalContext.current
+        Image(
+            painterResource(R.drawable.accuweather),
+            contentDescription = stringResource(R.string.powered_by_accuweather),
+            modifier = Modifier.clickable {
+                context.startActivity(Intent(Intent.ACTION_VIEW, ACCUWEATHER_URI))
+            },
+        )
+    }
+}
+
+private val ACCUWEATHER_URI = Uri.parse("https://www.accuweather.com")
+
+@Preview
+@Composable
+private fun ForecastScreenLoadingPreview() {
+    ForecastScreen(LoadState.Loading(), locationInput = "Searching, CA")
+}
+
+@Preview
+@Composable
+private fun ForecastScreenContentStatePreview() {
+    ForecastScreen(LoadState.Content(previewDataForecastResultSet), locationInput = "Some City, ST")
+}
+
+@Preview
+@Composable
+private fun ForecastScreenErrorPreview() {
+    val error = Error("An error occurred")
+    ForecastScreen(LoadState.Error(error), locationInput = "Nonexistent City")
+}
---
+++ app/src/test/java/io/github/tobyhs/weatherweight/forecast/ForecastScreenTest.kt
@@ -0,0 +1,148 @@
+package io.github.tobyhs.weatherweight.forecast
+
+import android.content.Intent
+
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.test.assertTextContains
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performImeAction
+import androidx.compose.ui.test.performTextInput
+import androidx.lifecycle.MutableLiveData
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import io.github.tobyhs.weatherweight.R
+import io.github.tobyhs.weatherweight.data.model.ForecastResultSet
+import io.github.tobyhs.weatherweight.test.BaseTestCase
+import io.github.tobyhs.weatherweight.test.ForecastResultSetFactory
+import io.github.tobyhs.weatherweight.ui.LoadState
+
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+
+@RunWith(AndroidJUnit4::class)
+class ForecastScreenTest : BaseTestCase() {
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    private val locationInputLiveData: MutableLiveData<String> = MutableLiveData("")
+    private val forecastStateLiveData: MutableLiveData<LoadState<ForecastResultSet>> = MutableLiveData()
+    private val viewModel: ForecastViewModel = mock {
+        on { locationInput } doReturn locationInputLiveData
+        on { forecastState } doReturn forecastStateLiveData
+    }
+
+    @Test
+    fun `locationInput initially set`() {
+        val location = "Initial Location"
+        locationInputLiveData.value = location
+        lateinit var inputContentDescription: String
+        composeRule.setContent {
+            ForecastScreen(viewModel)
+            inputContentDescription = stringResource(R.string.locationSearchHint)
+        }
+        composeRule.onNodeWithContentDescription(inputContentDescription)
+            .assertTextContains(location)
+    }
+
+    @Test
+    fun `entering a search on the keyboard`() {
+        lateinit var inputContentDescription: String
+        composeRule.setContent {
+            ForecastScreen(viewModel)
+            inputContentDescription = stringResource(R.string.locationSearchHint)
+        }
+        val locationInputNode = composeRule.onNodeWithContentDescription(inputContentDescription)
+        val location = "Search City"
+        locationInputNode.performTextInput(location)
+        assertThat(locationInputLiveData.value, equalTo(location))
+
+        locationInputNode.performImeAction()
+        verify(viewModel).search()
+    }
+
+    @Test
+    fun `clicking the search icon`() {
+        lateinit var buttonContentDescription: String
+        composeRule.setContent {
+            ForecastScreen(viewModel)
+            buttonContentDescription = stringResource(R.string.submitLocation)
+        }
+        composeRule.onNodeWithContentDescription(buttonContentDescription).performClick()
+        verify(viewModel).search()
+    }
+
+    @Test
+    fun initialState() {
+        setContent()
+        checkLce(null)
+    }
+
+    @Test
+    fun loading() {
+        forecastStateLiveData.value = LoadState.Loading()
+        setContent()
+        checkLce("loading")
+    }
+
+    @Test
+    fun content() {
+        forecastStateLiveData.value = LoadState.Content(ForecastResultSetFactory.create())
+        setContent()
+        checkLce("locationFound")
+        ForecastScreenContentTest.checkContent(composeRule)
+    }
+
+    @Test
+    fun error() {
+        val errorMessage = "Test Error"
+        forecastStateLiveData.value = LoadState.Error(Error(errorMessage))
+        setContent()
+        checkLce("error")
+        composeRule.onNodeWithTag("error").assertTextEquals(errorMessage)
+    }
+
+    @Test
+    fun `clicking the AccuWeather image`() {
+        Intents.init()
+        lateinit var accuweatherContentDescription: String
+        composeRule.setContent {
+            ForecastScreen(viewModel)
+            accuweatherContentDescription = stringResource(R.string.powered_by_accuweather)
+        }
+        composeRule.onNodeWithContentDescription(accuweatherContentDescription).performClick()
+        Intents.intended(
+            allOf(hasAction(Intent.ACTION_VIEW), hasData("https://www.accuweather.com"))
+        )
+    }
+
+    private fun setContent() {
+        composeRule.setContent { ForecastScreen(viewModel) }
+    }
+
+    private fun checkLce(expectedTestTag: String?) {
+        for (testTag in listOf("loading", "locationFound", "error")) {
+            val node = composeRule.onNodeWithTag(testTag)
+            if (testTag == expectedTestTag) {
+                node.assertExists()
+            } else {
+                node.assertDoesNotExist()
+            }
+        }
+    }
+}
