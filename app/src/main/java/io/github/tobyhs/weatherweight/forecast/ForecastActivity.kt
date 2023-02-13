95946abf2265617f8700efd3a12992052d3541f0
Toby Hsieh <thrasher13b@gmail.com>
2023-02-12 21:24:32 -0800

    Update ForecastActivity to use ForecastScreen composable

--- app/src/main/java/io/github/tobyhs/weatherweight/forecast/ForecastActivity.kt
+++ app/src/main/java/io/github/tobyhs/weatherweight/forecast/ForecastActivity.kt
@@ -1,113 +1,37 @@
 package io.github.tobyhs.weatherweight.forecast
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.SearchView
 
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
-
-import com.mikepenz.fastadapter.FastAdapter
-import com.mikepenz.fastadapter.adapters.ItemAdapter
 
 import dagger.hilt.android.AndroidEntryPoint
 
-import io.github.tobyhs.weatherweight.databinding.ActivityForecastBinding
 import io.github.tobyhs.weatherweight.di.ViewModelFactoryProducer
-import io.github.tobyhs.weatherweight.ui.LoadState
+import io.github.tobyhs.weatherweight.theme.AppTheme
 
-import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 /**
  * Main activity to enter a location to retrieve a weather forecast
  */
 @AndroidEntryPoint
-class ForecastActivity : AppCompatActivity(), SearchView.OnQueryTextListener, OnRefreshListener {
-    private lateinit var binding: ActivityForecastBinding
+class ForecastActivity : AppCompatActivity() {
     @set:Inject var viewModelFactoryProducer: ViewModelFactoryProducer? = null
     private lateinit var viewModel: ForecastViewModel
-    private lateinit var forecastItemAdapter: ItemAdapter<ForecastCardItem>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityForecastBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         viewModel = viewModels<ForecastViewModel>(factoryProducer = viewModelFactoryProducer).value
-        initializeViews()
-        setupObservers()
-
         if (savedInstanceState == null) {
             viewModel.loadLastForecast()
         }
-    }
 
-    override fun onQueryTextSubmit(query: String): Boolean {
-        viewModel.search()
-        return true
-    }
-
-    override fun onQueryTextChange(newText: String): Boolean {
-        if (newText != viewModel.locationInput.value) {
-            viewModel.locationInput.value = newText
-        }
-        return true
-    }
-
-    override fun onRefresh() {
-        viewModel.search()
-    }
-
-    /**
-     * Sets up views in the activity layout
-     */
-    private fun initializeViews() {
-        binding.locationSearch.setOnQueryTextListener(this)
-        binding.locationSearch.isSubmitButtonEnabled = true
-
-        forecastItemAdapter = ItemAdapter()
-        binding.forecastRecyclerView.layoutManager = LinearLayoutManager(this)
-        binding.forecastRecyclerView.adapter = FastAdapter.with(forecastItemAdapter)
-
-        binding.forecastSwipeContainer.setOnRefreshListener(this)
-        binding.accuweatherLogo.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, ACCUWEATHER_URI))
-        }
-    }
-
-    /**
-     * Registers LiveData observers
-     */
-    private fun setupObservers() {
-        viewModel.locationInput.observe(this) { location ->
-            binding.locationSearch.setQuery(location, false)
-        }
-
-        viewModel.forecastState.observe(this) { forecastState ->
-            if (forecastState is LoadState.Content) {
-                val forecastResultSet = forecastState.content
-                binding.locationFound.text = forecastResultSet.location
-                val pubDate = forecastResultSet.publicationTime
-                binding.pubDate.text = pubDate.format(DateTimeFormatter.RFC_1123_DATE_TIME)
-                binding.forecastSwipeContainer.isRefreshing = false
-                forecastItemAdapter.set(forecastResultSet.forecasts.map { dailyForecast ->
-                    ForecastCardItem(applicationContext, dailyForecast)
-                })
-            } else if (forecastState is LoadState.Error) {
-                binding.errorView.text = forecastState.error.message
+        setContent {
+            AppTheme {
+                ForecastScreen(viewModel)
             }
-
-            binding.loadingView.isVisible = forecastState is LoadState.Loading
-            binding.contentView.isVisible = forecastState is LoadState.Content
-            binding.errorView.isVisible = forecastState is LoadState.Error
         }
-    }
-
-    companion object {
-        private val ACCUWEATHER_URI = Uri.parse("https://www.accuweather.com")
     }
 }
--- app/src/test/java/io/github/tobyhs/weatherweight/forecast/ForecastActivityTest.kt
+++ app/src/test/java/io/github/tobyhs/weatherweight/forecast/ForecastActivityTest.kt
@@ -1,27 +1,10 @@
 package io.github.tobyhs.weatherweight.forecast
 
-import android.content.Intent
-
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
-import androidx.test.espresso.matcher.ViewMatchers.Visibility
-import androidx.test.espresso.matcher.ViewMatchers.hasChildCount
-import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
-import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withParentIndex
-import androidx.test.espresso.matcher.ViewMatchers.withResourceName
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import dagger.hilt.android.testing.BindValue
@@ -30,15 +13,8 @@ import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
 
 import io.github.tobyhs.weatherweight.R
-import io.github.tobyhs.weatherweight.data.model.ForecastResultSet
 import io.github.tobyhs.weatherweight.di.ViewModelFactoryProducer
 import io.github.tobyhs.weatherweight.di.ViewModelFactoryProducerModule
-import io.github.tobyhs.weatherweight.test.ForecastResultSetFactory
-import io.github.tobyhs.weatherweight.ui.LoadState
-
-import org.hamcrest.CoreMatchers.allOf
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.MatcherAssert.assertThat
 
 import org.junit.Rule
 import org.junit.Test
@@ -54,13 +30,12 @@ import org.mockito.kotlin.verify
 @UninstallModules(ViewModelFactoryProducerModule::class)
 @HiltAndroidTest
 class ForecastActivityTest {
-    @get:Rule val hiltRule = HiltAndroidRule(this)
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
 
-    private val locationInputLiveData: MutableLiveData<String> = MutableLiveData("")
-    private val forecastStatesLiveData: MutableLiveData<LoadState<ForecastResultSet>> = MutableLiveData()
     private val viewModel: ForecastViewModel = mock {
-        on { locationInput } doReturn locationInputLiveData
-        on { forecastState } doReturn forecastStatesLiveData
+        on { locationInput } doReturn MutableLiveData("")
+        on { forecastState } doReturn MutableLiveData()
     }
 
     @BindValue
@@ -68,17 +43,21 @@ class ForecastActivityTest {
         viewModelFactory { initializer { viewModel } }
     }
 
-    @get:Rule val activityScenarioRule = ActivityScenarioRule(ForecastActivity::class.java)
+    @get:Rule
+    var composeRule = createAndroidComposeRule<ForecastActivity>()
 
     @Test
     fun onCreate() {
-        checkLceVisibilities()
         verify(viewModel).loadLastForecast()
+        for (stringId in listOf(R.string.locationSearchHint, R.string.powered_by_accuweather)) {
+            val contentDescription = composeRule.activity.getString(stringId)
+            composeRule.onNodeWithContentDescription(contentDescription).assertExists()
+        }
     }
 
     @Test
     fun `onCreate with non-null savedInstanceState`() {
-        val scenario = activityScenarioRule.scenario
+        val scenario = composeRule.activityRule.scenario
         scenario.onActivity {
             // Clearing so we can check loadLastForecast isn't called on the 2nd onCreate
             clearInvocations(viewModel)
@@ -86,106 +65,5 @@ class ForecastActivityTest {
 
         scenario.recreate()
         scenario.onActivity { verify(viewModel, never()).loadLastForecast() }
-    }
-
-    @Test
-    fun `when viewModel locationInput changes`() {
-        val location = "VM location changed"
-        locationInputLiveData.value = location
-        onView(locationSearchInputMatcher).check(matches(withText(location)))
-    }
-
-    @Test
-    fun `when viewModel forecastState is Loading`() {
-        forecastStatesLiveData.value = LoadState.Loading()
-        checkLceVisibilities(R.id.loadingView)
-    }
-
-    @Test
-    fun `when viewModel forecastState is Content`() {
-        val forecastResultSet = ForecastResultSetFactory.create()
-        forecastStatesLiveData.value = LoadState.Content(forecastResultSet)
-
-        checkLceVisibilities(R.id.contentView)
-        onView(withId(R.id.locationFound)).check(matches(withText("Oakland, CA, US")))
-        onView(withId(R.id.pubDate)).check(matches(withText("Fri, 1 Feb 2019 12:00:00 GMT")))
-
-        val forecastLength = forecastResultSet.forecasts.size
-        onView(withId(R.id.forecastRecyclerView)).check(matches(hasChildCount(forecastLength)))
-
-        val firstForecastCardMatcher = allOf(withId(R.id.forecastCard), withParentIndex(0))
-        val descendantMatcher = isDescendantOfA(firstForecastCardMatcher)
-        onView(allOf(descendantMatcher, withId(R.id.day))).check(matches(withText("Fri")))
-        onView(allOf(descendantMatcher, withId(R.id.date))).check(matches(withText("Feb 1")))
-        onView(allOf(descendantMatcher, withId(R.id.temperatureLow))).check(matches(withText("60")))
-        onView(allOf(descendantMatcher, withId(R.id.temperatureHigh))).check(matches(withText("65")))
-        onView(allOf(descendantMatcher, withId(R.id.precipitationProbability))).check(matches(withText("10%")))
-        onView(allOf(descendantMatcher, withId(R.id.description))).check(matches(withText("Cloudy")))
-    }
-
-    @Test
-    fun `when viewModel forecastState is Error`() {
-        val errorMessage = "test error"
-        forecastStatesLiveData.value = LoadState.Error(Exception(errorMessage))
-        checkLceVisibilities(R.id.errorView)
-        onView(withId(R.id.errorView)).check(matches(withText(errorMessage)))
-    }
-
-    @Test
-    fun onQueryTextSubmit() {
-        onView(locationSearchInputMatcher).perform(typeText("Submit"))
-        onView(allOf(
-            isDescendantOfA(withId(R.id.locationSearch)),
-            withResourceName("search_go_btn")
-        )).perform(click())
-        verify(viewModel).search()
-    }
-
-    @Test
-    fun onQueryTextChange() {
-        val location = "Query Changed"
-        onView(locationSearchInputMatcher).perform(typeText(location))
-        assertThat(locationInputLiveData.value, equalTo(location))
-    }
-
-    @Test
-    fun onRefresh() {
-        // swipeDown doesn't work on SwipeRefreshLayout in Robolectric
-        // see https://github.com/robolectric/robolectric/issues/5375
-        activityScenarioRule.scenario.onActivity { activity ->
-            activity.onRefresh()
-            verify(viewModel).search()
-        }
-    }
-
-    @Test
-    fun `clicking the AccuWeather logo`() {
-        Intents.init()
-        onView(withId(R.id.accuweatherLogo)).perform(click())
-        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData("https://www.accuweather.com")))
-    }
-
-    /**
-     * Checks that the loading, content, and error views for the forecast result have the correct
-     * visibility.
-     *
-     * @param visibleResourceId resource ID of view that is expected to be visible
-     */
-    private fun checkLceVisibilities(visibleResourceId: Int? = null) {
-        for (resourceId in listOf(R.id.loadingView, R.id.contentView, R.id.errorView)) {
-            val expectedVisibility = if (resourceId == visibleResourceId) {
-                Visibility.VISIBLE
-            } else {
-                Visibility.GONE
-            }
-            onView(withId(resourceId)).check(matches(withEffectiveVisibility(expectedVisibility)))
-        }
-    }
-
-    companion object {
-        private val locationSearchInputMatcher = allOf(
-            isDescendantOfA(withId(R.id.locationSearch)),
-            withResourceName("search_src_text")
-        )
     }
 }
