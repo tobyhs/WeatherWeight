d7f9b0e16376a66a0c4085c26c1d00dfc63a63ca
Toby Hsieh <thrasher13b@gmail.com>
2024-08-26 00:26:00 -0700

    Refactor FileLastForecastCoroutinesStoreTest
    The setDispatcherAndRunTest method was unnecessary because I could pass
    testDispatcher to runTest.

--- app/src/test/java/io/github/tobyhs/weatherweight/storage/FileLastForecastCoroutinesStoreTest.kt
+++ app/src/test/java/io/github/tobyhs/weatherweight/storage/FileLastForecastCoroutinesStoreTest.kt
@@ -5,7 +5,6 @@ import io.github.tobyhs.weatherweight.di.AppModule
 import io.github.tobyhs.weatherweight.test.ForecastSearchFactory
 
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.runTest
 
 import org.hamcrest.CoreMatchers.equalTo
@@ -19,35 +18,28 @@ import java.io.File
 class FileLastForecastCoroutinesStoreTest {
     private val directory = File("/tmp")
     private val moshi = AppModule().provideMoshi()
-    private lateinit var testDispatcher: TestDispatcher
-    private val store by lazy { FileLastForecastCoroutinesStore(directory, moshi, testDispatcher) }
+    private val testDispatcher = StandardTestDispatcher()
+    private val store = FileLastForecastCoroutinesStore(directory, moshi, testDispatcher)
     private val forecastSearchAdapter by lazy { moshi.adapter(ForecastSearch::class.java) }
     private val file = File(directory, "lastForecast.json")
     private val forecastSearch by lazy { ForecastSearchFactory.create() }
 
     @Test
-    fun `get when file does not exist`() = setDispatcherAndRunTest {
+    fun `get when file does not exist`() = runTest(testDispatcher) {
         file.delete()
         assertThat(store.get(), nullValue())
     }
 
     @Test
-    fun `get when file exists`() = setDispatcherAndRunTest {
+    fun `get when file exists`() = runTest(testDispatcher) {
         file.writeText(forecastSearchAdapter.toJson(forecastSearch))
         assertThat(store.get(), equalTo(forecastSearch))
     }
 
     @Test
-    fun save() = setDispatcherAndRunTest {
+    fun save() = runTest(testDispatcher) {
         file.delete()
         store.save(forecastSearch)
         assertThat(forecastSearchAdapter.fromJson(file.readText()), equalTo(forecastSearch))
-    }
-
-    private fun setDispatcherAndRunTest(testBody: suspend () -> Unit) {
-        runTest {
-            this@FileLastForecastCoroutinesStoreTest.testDispatcher = StandardTestDispatcher(testScheduler)
-            testBody()
-        }
     }
 }
