c377e4c1254c6ff4b0228ba252b3218993e050ae
Toby Hsieh <thrasher13b@gmail.com>
2024-10-25 14:23:22 -0700

    Enable Gradle dependency locking

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
+allprojects {
+    dependencyLocking {
+        lockAllConfigurations()
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.hilt.android) apply false
