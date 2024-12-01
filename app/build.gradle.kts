675923db29f658245ab88a6191b18878f1479379
Toby Hsieh <thrasher13b@gmail.com>
2024-12-01 14:06:31 -0800

    Target Android SDK version 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,12 @@ rootProject.file("secret.properties").inputStream().use { secretProps.load(it) }
 
 android {
     namespace = "io.github.tobyhs.weatherweight"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.github.tobyhs.weatherweight"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
--- app/src/main/java/io/github/tobyhs/weatherweight/forecast/ForecastScreen.kt
+++ app/src/main/java/io/github/tobyhs/weatherweight/forecast/ForecastScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -69,6 +70,7 @@ private fun ForecastScreen(
 ) {
     Column(
         Modifier
+            .safeDrawingPadding()
             .padding(all = dimensionResource(R.dimen.forecast_screen_padding))
             .verticalScroll(rememberScrollState())
     ) {
